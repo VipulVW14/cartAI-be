ecbd81dfec92c856da3c2ec0cd1e11f995660fb1
Vipul <vipulwasnik0@gmail.com>
2024-09-23 23:03:06 +0530

    fix cors issue

--- index.js
+++ index.js
@@ -8,10 +8,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: 'https://cart-ai.netlify.app', // Specify your domain
+  origin: '*', // Specify your domain
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true, // Set this to true if sending cookies or credentials
 };
 
 app.use(cors(corsOptions));
