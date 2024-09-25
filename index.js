8c26de0d53dfe56efe6621d464301cf6a9a2f26c
Vipul <vipulwasnik0@gmail.com>
2024-09-25 18:28:28 +0530

    adds header in /events api

--- index.js
+++ index.js
@@ -212,6 +212,8 @@ app.get('/events', (req, res) => {
   res.setHeader('Content-Type', 'text/event-stream');
   res.setHeader('Cache-Control', 'no-cache');
   res.setHeader('Connection', 'keep-alive');
+  res.setHeader('Access-Control-Allow-Origin', 
+    'https://voicedemo.soluperts.com')
 
   // Add this client to the list of connected clients
   clients.push({ req, res });
